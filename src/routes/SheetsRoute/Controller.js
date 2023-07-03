7adb682e275188e0056ea5097ce0c64965f5746e
Marat252552 <117304418+Marat252552@users.noreply.github.com>
2023-07-03 19:09:11 +0300

    0.2

--- src/routes/SheetsRoute/Controller.js
+++ src/routes/SheetsRoute/Controller.js
@@ -1,26 +1,9 @@
 const { default: axios } = require('axios');
-const { google } = require("googleapis")
-
-const sheet_columns = [
-    { header: 'API', key: 'API', width: 15 },
-    { header: 'Description', key: 'Description', width: 15 },
-    { header: 'Auth', key: 'Auth', width: 15 },
-    { header: 'HTTPS', key: 'HTTPS', width: 15 },
-    { header: 'Cors', key: 'Cors', width: 15 },
-    { header: 'Category', key: 'Category', width: 15 },
-    { header: 'Link', key: 'Link', width: 15 },
-]
-
-const sortByLetters = (entry1, entry2) => {
-    const name1 = entry1.API.toUpperCase()
-    const name2 = entry2.API.toUpperCase()
-    if (name1 < name2) return -1
-    if (name1 > name2) return 1
-    return 0
-}
+const { google } = require("googleapis");
+const sortByLetters = require('../../shared/SortByLetters');
 
 class Controller {
-    async getSheets(req, res) {
+    async getSheets(_, res) {
         try {
 
             let { data: { entries } } = await axios.get('https://api.publicapis.org/entries')
@@ -35,13 +18,6 @@ class Controller {
             const googleSheets = google.sheets({ version: 'v4', auth: client })
 
             const spreadsheetId = process.env.SPREADSHEET_ID
-            // get metadata
-            // const metaData = await googleSheets.spreadsheets.get({
-            //     auth,
-            //     spreadsheetId,
-            // })
-
-            // write
             const formed_entries = entries.map(entry => {
                 const { API, Description, Auth, HTTPS, Cors, Category, Link } = entry
                 return [API, Description, Auth, HTTPS, Cors, Category, Link]
@@ -54,10 +30,9 @@ class Controller {
 
             const values = appendValue.map((e) => ({ userEnteredValue: { stringValue: e }, userEnteredFormat: { backgroundColor } }));
             const requests = [{ appendCells: { rows: [{ values }], sheetId: 0, fields: "userEnteredValue,userEnteredFormat" } }];
-            await googleSheets.spreadsheets.batchUpdate({ spreadsheetId, resource: { requests } });
+            googleSheets.spreadsheets.batchUpdate({ spreadsheetId, resource: { requests } });
 
-            // creating body
-            await googleSheets.spreadsheets.values.append({
+            googleSheets.spreadsheets.values.append({
                 auth,
                 spreadsheetId,
                 range,
